4fc99a426eddca58b0242b5ff5c24098fa8d5dd7
Antonioch9 <acallejah1994@gmail.com>
2018-05-24 13:55:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { NuevoJugadorComponent } from './jugadores/nuevo-jugador/nuevo-jugador.component';
+import { ListadoJugadorComponent } from './jugadores/listado-jugador/listado-jugador.component';
+import { EditarJugadorComponent } from './jugadores/editar-jugador/editar-jugador.component';
 
+
+const rutas: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'nuevo-jugador', component: NuevoJugadorComponent },
+  { path: 'listado-jugador', component: ListadoJugadorComponent },
+  { path: 'editar-jugador', component: EditarJugadorComponent },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    InicioComponent,
+    BarraLateralComponent,
+    NuevoJugadorComponent,
+    ListadoJugadorComponent,
+    EditarJugadorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
---
+++ src/app/jugadores/listado-jugador/listado-jugador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-jugador',
+  templateUrl: './listado-jugador.component.html',
+  styleUrls: ['./listado-jugador.component.css']
+})
+export class ListadoJugadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
